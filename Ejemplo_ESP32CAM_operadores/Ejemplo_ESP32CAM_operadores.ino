e7f1369a4aca71619f3fac68424eea139494e16d
arroyo78 <alexanderarroyon@gmail.com>
2022-04-08 10:23:45 -0500

    Update Ejemplo_ESP32CAM_operadores.ino

--- Ejemplo_ESP32CAM_operadores/Ejemplo_ESP32CAM_operadores.ino
+++ Ejemplo_ESP32CAM_operadores/Ejemplo_ESP32CAM_operadores.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Ejemplo de operador modulo
+ * Por: Alexander Arroyo
+ * Fecha: 8 de abril de 2022
+ * 
+ * Este es un programa para ejemplificar el uso básico del lengiaje C
+ * y el microcontrolador ESP32CAM
+ * 
+ */
 
-}
+// Bibliotecas
 
-void loop() {
+// Constantes
+
+// Variables
+
+// Definición de objetos
+
+// Condiciones iniciales - Se ejecuta sólo una vez al energizar
+void setup() { //Inicio de void setup ()
+  // Aquí va tu código
+
+}// Fin de void setup
+
+//Cuerpo del programa - Se ejecuta constantemente
+void loop() {// Inicio de void loop
   // put your main code here, to run repeatedly:
 
-}
+}//Fin de void loop
+
+// Funciones del usuario
